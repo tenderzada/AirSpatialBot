1aa00c5d89c5d3b4cf82ff8e721fb410b7b6e45c
tenderzada <277198552@qq.com>
2025-11-29 08:31:10 +0800

    update

--- start_huav_lora_socket.sh
+++ start_huav_lora_socket.sh
@@ -11,7 +11,7 @@ echo "============================================================"
 # Configuration
 MODEL_PATH="/mnt/data/AirSpatialBot"
 VISION_TOWER="/mnt/data/clip-vit-large-patch14-336"
-LORA_WEIGHTS="./outputs/lora_injection_sqa/lora_adapters_best.pt"
+LORA_WEIGHTS="./outputs/lora_injection_sqa/lora_adapters_epochbest.pt"
 PORT=50051
 DEVICE="cuda:0"
 
